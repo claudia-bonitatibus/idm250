47ba57ded3789f7a8b1e8261087d83706ccac7db
claudia-bonitatibus <32339275+claudia-bonitatibus@users.noreply.github.com>
2018-08-13 21:12:02 -0400

    Add files via upload

---
+++ wp-content/static/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	content = "width= device-width,
+	initial-scale= 1.0"
+	>
+	<title>Claudia Bonitatibus</title>
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/5.0.0/normalize.css">
+	<link rel="stylesheet" href="style-sheet.css">
+	</head>
+	<body>
+		<nav>
+			<div class="search">
+			<input type="text" class="inputChannel" placeholder="Search...">
+				<a href="results.html">
+					<img src = assets/search-button.svg alt="search" class="searchButton">
+				</a>
+			</div>
+		</nav>
